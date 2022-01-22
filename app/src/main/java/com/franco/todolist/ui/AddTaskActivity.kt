03a4dc69449589969970a20671c72c10644ff002
evanvidel <evanvidel@gmail.com>
2022-01-22 18:46:11 -0300

    criação da tela de criar tarefas

--- app/src/main/java/com/franco/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/franco/todolist/ui/AddTaskActivity.kt
@@ -1,11 +1,12 @@
-package com.franco.todolist
+package com.franco.todolist.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.franco.todolist.R
 
-class MainActivity : AppCompatActivity() {
+class AddTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_add_task)
     }
 }
---
+++ app/src/main/java/com/franco/todolist/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.franco.todolist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.franco.todolist.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
